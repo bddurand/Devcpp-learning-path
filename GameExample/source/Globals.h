108c485842f1db399d6a6814780c26bcf25e6687
bddurand <brunodurand3@gmail.com>
2023-08-17 18:59:04 -0300

    feat(Sopa-de-letras):
    * Diccionario de palabras para la grilla
    * Game loop
    * Uso de Strings

--- GameExample/source/Globals.h
+++ GameExample/source/Globals.h
@@ -2,9 +2,11 @@
 
 #include <iostream>
 #include <time.h>
+#include <string>
 
 const int DIMENSION = 10;
-const int MAX_WORDS = 7;
+const int MAX_WORDS = 3;
+const int MAX_WORDS_PER_DICTIONARY = 5;
 
 struct Block {
 
@@ -16,8 +18,10 @@ struct Block {
 
 struct Word {
 
-	const char* data;
+	std::string data;
 	int size;
+	bool wasFound;
+	bool wasInserted;
 };
 
 
@@ -31,3 +35,15 @@ enum Orientation {
 	Vertically = 1, 
 	Diagonally = 2
 };
+
+const std::string heroes[MAX_WORDS_PER_DICTIONARY] = {
+	"SUPERMAN",
+	"BATMAN",
+	"FLASH",
+	"ROBIN",
+	"AQUAMAN"
+};
+
+void display(const std::string& message) {
+	std::cout << message << std::endl;
+}
--- GameExample/source/Main.cpp
+++ GameExample/source/Main.cpp
@@ -1,9 +1,8 @@
 #include "Globals.h"
 
-
 void insertWord(Block** grid, Word word, int start_x, int start_y, Orientation orientation) {
 
-	const char* data = word.data;
+	const std::string data = word.data;
 	int size = word.size;
 
 	for (int offset = 0; offset < size; offset++) {
@@ -14,21 +13,21 @@ void insertWord(Block** grid, Word word, int start_x, int start_y, Orientation o
 
 		switch (orientation) {
 		
-			case 0: //horizontal
+			case 0: //horizontally
 				block.x_position = start_x;
 				block.y_position = start_y + offset;
 
 				grid[start_y][start_x + offset] = block;
 				break;
 		
-			case 1: //vertical
+			case 1: //vertically
 				block.x_position = start_x + offset;
 				block.y_position = start_y;
 
 				grid[start_y + offset][start_x] = block;
 				break;
 
-			case 2:
+			case 2: //diagonally
 				block.x_position = start_x + offset;
 				block.y_position = start_y + offset;
 
@@ -44,7 +43,7 @@ bool canInsertWord(Block** grid, Word word, int start_x, int start_y, Orientatio
 	int size = word.size;
 
 	if (DIMENSION < size ) {
-		std::cout << "No puedo insertar: palabra mas grande que grilla" << std::endl;
+		display("No puedo insertar: palabra mas grande que grilla");
 		return false;
 	}
 
@@ -115,54 +114,57 @@ bool canInsertWord(Block** grid, Word word, int start_x, int start_y, Orientatio
 void printGrid(Block** grid) {
 
 
-	std::cout << std::endl;
+	display("\n");
+	display("***********************");
 
 	for (int i = 0; i < DIMENSION; i++) {
-
 		for (int j = 0; j < DIMENSION; j++) {
-
 			std::cout << grid[i][j].data << " ";
 		}
-
 		std::cout << std::endl;
 	}
+
+	display("***********************");
+	display("\n\n");
+
 }
 
-int main() {
-
-	srand(time(NULL));
-	Word words[MAX_WORDS] = { };
-
-	Block** grid = new Block* [DIMENSION] {};
-
-
-	//Initialize Grid
+void initializeGrid(Block** grid) {
 	for (int i = 0; i < DIMENSION; i++) {
 		grid[i] = new Block[DIMENSION];
 
 		for (int j = 0; j < DIMENSION; j++) {
 			Block block;
-			block.data = '*'; //replace with random letter
+			block.data = '*';//getRandomChar();
 			block.x_position = 0;
 			block.y_position = 0;
 			block.isWord = false;
 			grid[i][j] = block;
 		}
 	}
+}
 
-	printGrid(grid);
+std::string getRandomEntry() {
+	srand(time(NULL));
+	int i = rand() % MAX_WORDS_PER_DICTIONARY - 0;
+	return heroes[i];
+}
 
-
-	//Initialize Words
+void initializeWords(Word* words) {
+	
 	for (int i = 0; i < MAX_WORDS; i++) {
 		Word word;
-		word.data = "asd";
-		word.size = (int) strlen(word.data);
+		word.data = getRandomEntry();
+		word.size = word.data.length();
+		word.wasFound = false;
+		word.wasInserted = false;
 
 		words[i] = word;
 	}
+}
 
-	//Insert Words into Grid
+void fillGrid(Block** grid, Word* words) {
+
 	for (int i = 0; i < MAX_WORDS; i++) {
 
 		int x = rand() % DIMENSION - 0;
@@ -170,11 +172,87 @@ int main() {
 
 		Orientation orientation = static_cast<Orientation> (rand() % 3);
 
-		if(canInsertWord(grid, words[i], x, y, orientation))
+		if (canInsertWord(grid, words[i], x, y, orientation)) {
 			insertWord(grid, words[i], x, y, orientation);
+			words[i].wasInserted = true;
+		}
+	}
+}
+
+bool search(Block** grid, Word* words, std::string& word) {
+	for (int i = 0; i < MAX_WORDS; i++) {
+		if (word.compare(words[i].data) == 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+int count(Word* words) {
+	int inserted = 0;
+
+	for (int i = 0; i < MAX_WORDS; i ++) {
+		if (words[i].wasInserted)
+			inserted++;
 	}
 
+	return inserted;
+}
+
+int main() {
+
+	srand(time(NULL));
+
+	Word words[MAX_WORDS] = {};
+	Block** grid = new Block* [DIMENSION] {};
+	bool mustContinue = true;
+	std::string input = "";
+	int tries = 3;
+	int found = 0;
+	int inserted = 0;
+
+	display("<<<<< SOPA DE LETRAS! >>>>>");
+	display(">> Este juego consiste en una grilla de 10x10.");
+	display(">> Hay palabras ocultas que deberas encontrar.");
+	display(">> Tenes 3 intentos.");
+	display(">> Presione 'ENTER' para iniciar.");
+	std::cin.get();
+
+	initializeGrid(grid);
+	initializeWords(words);
+
+	fillGrid(grid, words);
+	inserted = count(words);
+
 	printGrid(grid);
+	
+	while(mustContinue) {
+
+		if (found < inserted) {
+
+			display(">> Inserte una palabra para buscar: ");
+			std::cin >> input;
+			bool correct = search(grid, words, input);
+			if (correct) {
+				found++;
+				display(">> Bien! Ya encontraste: " + std::to_string(found) + " de " + std::to_string(inserted) + " palabras.\n");
+			}
+			else {
+				tries--;
+				display(">> Que pena! :( La respuesta es incorrecta. Te quedan: " + std::to_string(tries) + " intentos.");
+				display(">> Desea continuar buscando? Si/No: ");
+				std::cin >> input;
+				if (input == "no")
+					mustContinue = false;
+			}
+		}
+		else {
+			display(">> GANASTE! :) Encontraste todas las palabras.\n");
+			display(">> Resultado: ");
+			printGrid(grid);
+			mustContinue = false;
+		}
+	}
 
 	std::cin.get();
 }
